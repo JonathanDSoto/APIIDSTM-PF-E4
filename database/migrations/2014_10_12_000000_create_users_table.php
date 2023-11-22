07e9b2ec2dca48eeda62e2d7416ea656e3173558
LVelcar <luisvelcar9@gmail.com>
2023-11-22 09:49:54 -0700

    Creación de modelos y ajuste en la migración create_users_table.php
    Se crearon los modelos base y se agregó el campo de "lastname" en la migración de create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lastname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            //$table->foreign('rol_id')->references('id')->on('roles');
             $table->rememberToken();
             $table->timestamps();
         });

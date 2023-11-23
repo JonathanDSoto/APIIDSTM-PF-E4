47e09bc12535d61bfa70f3007f25f4a70320fee9
LVelcar <luisvelcar9@gmail.com>
2023-11-22 18:19:38 -0700

    Models, Seeders, Controllers and Migrations
    Se realizó la creación de los Models, Seeders, Controllers y Migrations

---
+++ app/Http/Controllers/SubjectSelectionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subject_Selection;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SubjectSelectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Subject_Selection $subject_Selection)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Subject_Selection $subject_Selection)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Subject_Selection $subject_Selection)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Subject_Selection $subject_Selection)
+    {
+        //
+    }
+}

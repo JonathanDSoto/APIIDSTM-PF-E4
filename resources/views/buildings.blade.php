c02b01f1f0c5409e625281c5f1e670c031acbee7
Emiliano Fernández <emilianofh02@gmail.com>
2023-11-29 22:34:10 -0700

    Modificación en app-user-list.js

--- resources/views/buildings.blade.php
+++ resources/views/buildings.blade.php
@@ -76,24 +76,20 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel1">Modal title</h5>
+                        <h5 class="modal-title" id="exampleModalLabel1">Editar Edificio</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
                         <div class="row">
                             <div class="col mb-3">
-                                <label for="nameBasic" class="form-label">Name</label>
-                                <input type="text" id="nameBasic" class="form-control" placeholder="Enter Name">
+                                <label for="nameBasic" class="form-label">Identificador</label>
+                                <input type="text" id="nameBasic" class="form-control" placeholder="Ingresa el identificador">
                             </div>
                         </div>
-                        <div class="row g-2">
-                            <div class="col mb-0">
-                                <label for="emailBasic" class="form-label">Email</label>
-                                <input type="email" id="emailBasic" class="form-control" placeholder="xxxx@xxx.xx">
-                            </div>
-                            <div class="col mb-0">
-                                <label for="dobBasic" class="form-label">DOB</label>
-                                <input type="date" id="dobBasic" class="form-control">
+                        <div class="row">
+                            <div class="col mb-3">
+                                <label for="nameBasic" class="form-label">Nombre</label>
+                                <input type="text" id="nameBasic" class="form-control" placeholder="Ingresa el nombre">
                             </div>
                         </div>
                     </div>

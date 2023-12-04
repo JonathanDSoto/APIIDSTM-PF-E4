60ad9284d2386e18eb6dc7243a45950b58da70be
Emiliano Fernández <emilianofh02@gmail.com>
2023-12-03 20:33:43 -0700

    Vista de Iniciativas
    Se realiza la maquetación de la vista de "Iniciativas" y se hacen pequeños ajustes en calendar.blade.php

--- resources/views/initiative.blade.php
+++ resources/views/initiative.blade.php
@@ -5,6 +5,7 @@
 
 @section('aditional_header')
     
+    
 @endsection
 
 @section('aditional_scripts')
@@ -23,147 +24,100 @@
     <link rel="stylesheet" href="../../assets/vendor/libs/bootstrap-daterangepicker/bootstrap-daterangepicker.css" />
     <link rel="stylesheet" href="../../assets/vendor/libs/jquery-timepicker/jquery-timepicker.css" />
     <link rel="stylesheet" href="../../assets/vendor/libs/pickr/pickr-themes.css" />
-    
+
+   
     <!-- Row Group CSS -->
-  <link rel="stylesheet" href="../../assets/vendor/libs/datatables-rowgroup-bs5/rowgroup.bootstrap5.css">
-  <!-- Form Validation -->
-  <link rel="stylesheet" href="../../assets/vendor/libs/@form-validation/umd/styles/index.min.css" />
+    <link rel="stylesheet" href="../../assets/vendor/libs/datatables-rowgroup-bs5/rowgroup.bootstrap5.css">
+    <!-- Form Validation -->
+    <link rel="stylesheet" href="../../assets/vendor/libs/@form-validation/umd/styles/index.min.css" />
+
+    <style>
+      .delete-button {
+        padding: 2px 2px;
+      }
+
+      .delete-button i {
+        font-size: 1.2rem;
+      }
+
+      .delete-button:hover {
+        color: #fff;
+        background-color: #dc3545; 
+        border-color: #dc3545; 
+      }
+    </style>
+
+ 
 @endsection
 
 @section('content')            
 <h4 class="py-3 mb-4">
   <span class="text-muted fw-light">UBACS</span> Iniciativas
 </h4>
- 
-  <!-- Basic Bootstrap Table -->
-<div class="card">
-  <h5 class="card-header">Lista de Iniciativas</h5>
-  <div class="table-responsive text-nowrap">
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Iniciativa</th>
-          <th>Nombre</th>
-          <th>Usuario</th>
-          <th>Estado</th>
-          <th>Acciones</th>
-        </tr>
-      </thead>
-      <tbody class="table-border-bottom-0">
-        <tr>
-          <td><i class="ti ti-brand-angular ti-lg text-danger me-3"></i> <span class="fw-medium">Angular Project</span></td>
-          <td>Albert Cook</td>
-          <td>
-            <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Lilian Fuller">
-                <img src="../../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle">
-              </li>
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Sophia Wilkerson">
-                <img src="../../assets/img/avatars/6.png" alt="Avatar" class="rounded-circle">
-              </li>
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Christina Parker">
-                <img src="../../assets/img/avatars/7.png" alt="Avatar" class="rounded-circle">
-              </li>
-            </ul>
-          </td>
-          <td><span class="badge bg-label-primary me-1">Active</span></td>
-          <td>
-            <div class="dropdown">
-              <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical"></i></button>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-1"></i> Delete</a>
-              </div>
+
+<?php
+$iniciativas = [
+    ['titulo' => 'Angular Project', 'descripcion' => 'Breve descripción del proyecto...', 'usuario' => 'Usuario 1',
+     'estado' => 'Aprobada', 'imagen' => '../../assets/img/elements/10.jpg', 'usuario_img' => '../../assets/img/avatars/5.png'],
+    ['titulo' => 'Validar Horarios', 'descripcion' => 'Breve descripción del proyecto...', 'usuario' => 'Usuario 2',
+    'estado'=> 'Aprobada', 'imagen' => '../../assets/img/elements/12.jpg', 'usuario_img' => '../../assets/img/avatars/6.png'],
+    ['titulo' => 'Limpiar Baños', 'descripcion' => 'Breve descripción del proyecto...', 'usuario' => 'Usuario 3',
+    'estado'=> 'Aprobada', 'imagen' => '../../assets/img/elements/13.jpg', 'usuario_img' => '../../assets/img/avatars/7.png']
+  ];
+?>
+
+<!-- Sección de Iniciativas -->
+<div class="row">
+    <?php foreach ($iniciativas as $iniciativa) : ?>
+        <div class="col-md-4 mb-4">
+            <div class="card ios-card position-relative">
+                <div class="position-absolute top-0 end-0 m-4">
+                    <button type="button" data-bs-toggle="modal" data-bs-target="#deleteModal" class="btn btn-outline-danger delete-button">
+                        <i class="ti ti-trash me"></i>
+                    </button>
+                </div>
+
+                <h5 class="card-header ios-header"><strong><?php echo $iniciativa['titulo']; ?></strong></h5>
+                <div class="card-body ios-body">
+                    <div class="text-center mb-2">
+                        <img src="<?php echo $iniciativa['imagen']; ?>" alt="Imagen de iniciativa" class="img-fluid">
+                    </div>
+                    <p class="card-text ios-text"><strong>Descripción: </strong><?php echo $iniciativa['descripcion']; ?></p>
+                    <div class="d-flex align-items-center mb-2">
+                        <p class="card-text ios-text me-2"><strong>Usuario: </strong></p>
+                        <img src="<?php echo $iniciativa['usuario_img']; ?>" alt="Avatar" class="rounded-circle m-2" style="width: 32px; height: 32px;">
+                        <p><?php echo $iniciativa['usuario']; ?></p>
+                    </div>
+                    <p class="card-text ios-text"><strong>Estado: </strong> <span class="badge bg-label-primary"><?php echo $iniciativa['estado']; ?></span></p>
+                </div>
             </div>
-          </td>
-        </tr>
-        <tr>
-          <td><i class="ti ti-brand-react-native ti-lg text-info me-3"></i> <span class="fw-medium">React Project</span></td>
-          <td>Barry Hunter</td>
-          <td>
-            <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Lilian Fuller">
-                <img src="../../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle">
-              </li>
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Sophia Wilkerson">
-                <img src="../../assets/img/avatars/6.png" alt="Avatar" class="rounded-circle">
-              </li>
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Christina Parker">
-                <img src="../../assets/img/avatars/7.png" alt="Avatar" class="rounded-circle">
-              </li>
-            </ul>
-          </td>
-          <td><span class="badge bg-label-success me-1">Completed</span></td>
-          <td>
-            <div class="dropdown">
-              <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical"></i></button>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-2"></i> Delete</a>
-              </div>
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td><i class="ti ti-brand-vue ti-lg text-success me-3"></i> <span class="fw-medium">VueJs Project</span></td>
-          <td>Trevor Baker</td>
-          <td>
-            <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Lilian Fuller">
-                <img src="../../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle">
-              </li>
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Sophia Wilkerson">
-                <img src="../../assets/img/avatars/6.png" alt="Avatar" class="rounded-circle">
-              </li>
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Christina Parker">
-                <img src="../../assets/img/avatars/7.png" alt="Avatar" class="rounded-circle">
-              </li>
-            </ul>
-          </td>
-          <td><span class="badge bg-label-info me-1">Scheduled</span></td>
-          <td>
-            <div class="dropdown">
-              <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical"></i></button>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-2"></i> Delete</a>
-              </div>
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td><i class="ti ti-brand-bootstrap ti-lg text-primary me-3"></i> <span class="fw-medium">Bootstrap Project</span></td>
-          <td>Jerry Milton</td>
-          <td>
-            <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Lilian Fuller">
-                <img src="../../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle">
-              </li>
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Sophia Wilkerson">
-                <img src="../../assets/img/avatars/6.png" alt="Avatar" class="rounded-circle">
-              </li>
-              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Christina Parker">
-                <img src="../../assets/img/avatars/7.png" alt="Avatar" class="rounded-circle">
-              </li>
-            </ul>
-          </td>
-          <td><span class="badge bg-label-warning me-1">Pending</span></td>
-          <td>
-            <div class="dropdown">
-              <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical"></i></button>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-2"></i> Delete</a>
-              </div>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+        </div>
+    <?php endforeach; ?>
+</div>
+
+
+  <!-- Modal de confirmación para eliminar -->
+  <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="deleteModalLabel">Confirmar Eliminación</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          ¿Estás seguro de que deseas eliminar esta iniciativa?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+          <!-- Agregamos el botón de eliminar con el ícono de bote de basura -->
+          <button type="button" class="btn btn-danger">
+            <i class="bi bi-trash"></i> Eliminar
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
-</div>
-
-
-  
-<!--/ Basic Bootstrap Table -->
-
 
   <!-- Vendors JS -->
     <script src="../../assets/vendor/libs/bs-stepper/bs-stepper.js"></script>
@@ -182,6 +136,5 @@
     <script src="../../assets/js/form-layouts.js"></script>
     <script src="../../assets/js/form-wizard-numbered.js"></script>
     <script src="../../assets/js/form-wizard-validation.js"></script>
-
 
 @endsection
